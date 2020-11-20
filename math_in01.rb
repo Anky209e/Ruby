1e22f6dfff11630f8c8a3cde6cafc55b67b09bfa
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-20 21:36:32 +0530

    Mathematical functions of ruby

---
+++ math_in01.rb
@@ -0,0 +1,10 @@
+#power
+# puts 2**3
+num = 2
+#here to_s is used for converting num in string
+puts("My fav num is"+num.to_s)
+# round off numbers
+num2 = 20.09
+puts num2.round()
+# take a square of a number
+puts Math.sqrt(36)
