11186370b761bc49e1689c5db567d0da520f9620
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-20 21:58:56 +0530

    If else in ruby

---
+++ if_stats_01.rb
@@ -0,0 +1,13 @@
+isfriend = true
+isgood = true
+ismale = false
+if isfriend and isgood
+    puts "You are my good friend"
+elsif ismale and isgood
+    puts "You are girlfriend"
+else 
+    puts "You are not my friend and not good"
+end
+if isgood
+    puts("good friend")
+end
