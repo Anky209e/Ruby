e689e597355e91f7a6e7826f4a5de5886a98dfb6
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-20 20:56:48 +0530

    Strings and its functions

---
+++ strings_01.rb
@@ -0,0 +1,17 @@
+#more on strings
+phrase = "ankit its my name"
+puts phrase
+#for capitilize letter
+puts phrase.upcase()
+#for lowercase letter
+# puts phrase.lowercase()
+#for no of charachters
+puts phrase.length()
+#to check if a letter or word exist
+puts phrase.include? "ankit"
+#string slicing like python
+puts phrase[0,5]
+#tells us about position of letter
+puts phrase.index("k")
+#make a word uppercase
+puts "ankit".upcase()
