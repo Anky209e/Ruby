09a9310a4c9ac423b711743098d8d966e968fedb
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-20 21:38:02 +0530

    like dictionry of py

---
+++ hases_01.rb
@@ -0,0 +1,6 @@
+# these are like dictinory of python
+friends = {
+    "Dingo" => "gamer","capti" => "programmming","silver" => "singing","ankit" => "hacking"
+}
+puts friends
+puts friends["Dingo"]
