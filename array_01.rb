d066c54670d568211927156d0e2f78bf8498b50c
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-20 21:37:10 +0530

    Array in ruby

---
+++ array_01.rb
@@ -0,0 +1,6 @@
+friends = Array["capti","silver","dingo","chandler","capricorn"]
+puts friends
+# You can store all type of data type in strings like boolean,numbers etc.
+# array slicing
+puts friends[0,3]
+puts friends[-2]
