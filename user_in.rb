0f7d106f947b6d44b30a0c21bb7f8ee8473c60c3
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-22 13:36:13 +0530

    User input in ruby

---
+++ user_in.rb
@@ -0,0 +1,8 @@
+puts "hello user Enter your name"
+user_name = gets.chomp()
+puts ("hello "+user_name+" u are cool!")
+puts ("Enter your age")
+user_age = gets.chomp()
+puts (user_name+" your age is "+user_age)
+
+# chomp is used foe same line
