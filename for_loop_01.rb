d534ec68599e78351e5ba42b2a8adba48f61f97a
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-24 18:45:54 +0530

    For loops in ruby

---
+++ for_loop_01.rb
@@ -0,0 +1,17 @@
+friends = ["ankit","darshan","karan","shomil"]
+# 1st method
+for element in friends
+    puts element
+end
+# 2nd method
+friends.each do |element|
+    puts element
+end
+# 1st method for numbers
+for index in 0..5
+    puts index
+end
+# 2nd methods for numbers
+11.times do |numbers|
+    puts numbers
+end
