996542b4cbd40ee410f2e44f21f223f36fbb9c2b
Ankit Kumar <71585377+Anky209e@users.noreply.github.com>
2020-11-24 19:15:24 +0530

    Writing and creating file in ruby

---
+++ write_file_01.rb
@@ -0,0 +1,8 @@
+# f = File.open("read.txt","a")
+# f.write("\nhello i am ankit kumar i appended this")
+
+# f.close
+# how to create a file and write
+f = File.open("byruby.txt","w")
+f.write("print")
+f.close()
